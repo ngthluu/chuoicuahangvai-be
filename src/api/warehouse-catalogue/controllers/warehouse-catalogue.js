867349c31af729533651fb126339ea34178346e0
Luu <ngthluu@gmail.com>
2022-03-20 16:02:43 +0700

    Warehouse catalogue data structure

---
+++ src/api/warehouse-catalogue/controllers/warehouse-catalogue.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  warehouse-catalogue controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::warehouse-catalogue.warehouse-catalogue');
---
+++ src/api/warehouse-catalogue/routes/warehouse-catalogue.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * warehouse-catalogue router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::warehouse-catalogue.warehouse-catalogue');
