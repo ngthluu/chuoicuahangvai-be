a12e955a562730474bc3982122c6a5ff7900e94c
Luu <ngthluu@gmail.com>
2022-03-20 12:54:37 +0700

    Finish user services

--- src/api/user/services/user.js
+++ src/api/user/services/user.js
@@ -11,7 +11,7 @@ module.exports = () => ({
             where: {
                 role: {
                     name: {
-                        $notIn: ['Super Admin']
+                        $notIn: ['Super Admin', 'Customer']
                     }
                 },
                 ...params
