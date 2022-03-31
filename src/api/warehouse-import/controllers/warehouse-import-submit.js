eda84b4ef502ada004a32dfadcd7de0dbc65b34c
Luu <ngthluu@gmail.com>
2022-03-31 16:55:53 +0700

    Finish import submit

--- src/api/warehouse-import/controllers/warehouse-import-submit.js
+++ src/api/warehouse-import/controllers/warehouse-import-submit.js
@@ -7,6 +7,9 @@
 module.exports = {
   submitImport: async (ctx, next) => {
     try {
+      const { id } = ctx.params;
+      const { user } = ctx.state;
+      await strapi.service('api::warehouse-import.warehouse-import-submit').submitImport(id, user.id); 
       ctx.body = 'ok';
     } catch (err) {
       ctx.body = err;
--- src/api/warehouse-import/services/warehouse-import-submit.js
+++ src/api/warehouse-import/services/warehouse-import-submit.js
@@ -4,4 +4,33 @@
  * warehouse-import-submit service.
  */
 
-module.exports = () => ({});
+module.exports = () => ({
+    async submitImport(id, userId) {
+        const importData = await strapi.entityService.findOne('api::warehouse-import.warehouse-import', id, {
+            fields: ['id', 'note'],
+            populate: ['branch', 'products', 'products', 'products.sku'],
+        });
+        // Update data to inventory table
+        importData.products.forEach(item => {
+            for (let i = 0; i < item.quantity; i++) {
+                strapi.entityService.create('api::warehouse-inventory.warehouse-inventory', {
+                    data: {
+                        sku_quantity: {
+                            sku: item.sku.id,
+                            length: item.length,
+                            quantity: 1,
+                        }
+                    }
+                });
+            }
+        });
+        // Update import status
+        strapi.entityService.update('api::warehouse-import.warehouse-import', id, {
+            data: {
+                submit_status: true,
+                submit_user: userId,
+                submit_time: (new Date()).toISOString(),
+            }
+        });
+    }
+});
