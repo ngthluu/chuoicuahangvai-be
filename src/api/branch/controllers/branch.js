bc84c4ba281a37a043f5647173ceb49279df1abe
Luu <ngthluu@gmail.com>
2022-04-23 14:41:39 +0700

    Customize branch

--- src/api/branch/controllers/branch.js
+++ src/api/branch/controllers/branch.js
@@ -6,4 +6,13 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::branch.branch');
+module.exports = createCoreController('api::branch.branch', ({strap}) => ({
+    async find(ctx) {
+        ctx.query = {
+            ...ctx.query,
+            user: ctx.state.user
+        }
+        const { data, meta } = await super.find(ctx)
+        return { data, meta };
+    }
+}));
--- src/api/branch/services/branch.js
+++ src/api/branch/services/branch.js
@@ -6,4 +6,23 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::branch.branch');
+module.exports = createCoreService('api::branch.branch', ({strapi}) => ({
+    async find(args) {
+        const { user, ...otherArgs } = args
+        const fetchUser = await strapi.service('api::user.user').fetchOne(user.id, [], ['branches'])
+        let fetchArgs = {...otherArgs}
+        if (fetchUser) {
+            fetchArgs = {
+                ...fetchArgs,
+                filters: { 
+                    id: { 
+                        $in: fetchUser.branches.map((item) => item.id) 
+                    } 
+                }
+            }
+        }
+        strapi.log.info(JSON.stringify(fetchArgs));
+        const { results, pagination } = await super.find(fetchArgs);
+        return { results, pagination }
+    }
+}));
