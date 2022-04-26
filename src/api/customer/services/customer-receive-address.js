fd784639e2052377ecddefc9a2fb3038e6a397b4
Luu <ngthluu@gmail.com>
2022-04-26 23:02:43 +0700

    Finish customer add/update/remove

--- src/api/customer/services/customer-receive-address.js
+++ src/api/customer/services/customer-receive-address.js
@@ -23,14 +23,67 @@ module.exports = () => ({
     },
 
     async addReceiveAddress(userId, data) {
-
+      const {firstname, lastname, phone, address, ward_id, is_default} = data;
+      const userData = await getUserData(userId);
+      await strapi.entityService.update('plugin::users-permissions.user', userData.id, {
+        data: {
+          receive_address: [
+            ...userData.receive_address,
+            {
+              name: {
+                firstname: firstname,
+                lastname: lastname,
+              },
+              address: {
+                address: address,
+                address_three_levels: ward_id,
+              },
+              phone: phone,
+              is_default: is_default,
+            },
+          ],
+        },
+      });
+      return true;
     },
 
     async updateReceiveAddress(id, userId, data) {
-
+      const {firstname, lastname, phone, address, ward_id, is_default} = data;
+      const userData = await getUserData(userId);
+      const receiveAddressData = userData.receive_address.map((item) => {
+        if (item.id == id) {
+          return {
+            name: {
+              firstname: firstname,
+              lastname: lastname,
+            },
+            address: {
+              address: address,
+              address_three_levels: ward_id,
+            },
+            phone: phone,
+            is_default: is_default,
+          };
+        } else {
+          return item;
+        }
+      });
+      await strapi.entityService.update('plugin::users-permissions.user', userData.id, {
+        data: {
+          receive_address: receiveAddressData,
+        },
+      });
+      return true;
     },
 
     async deleteReceiveAddress(id, userId) {
-      
+      const userData = await getUserData(userId);
+      const receiveAddressData = userData.receive_address.filter((item) => item.id != id);
+      await strapi.entityService.update('plugin::users-permissions.user', userData.id, {
+        data: {
+          receive_address: receiveAddressData,
+        },
+      });
+      return true;
     },
 });
