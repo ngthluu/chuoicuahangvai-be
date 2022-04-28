2479f119ae3d881165f73152ae10cbedfa688624
Luu <ngthluu@gmail.com>
2022-04-28 16:46:12 +0700

    Finish cart step complete

--- src/api/cart/services/cart-step-complete.js
+++ src/api/cart/services/cart-step-complete.js
@@ -1,5 +1,49 @@
 'use strict';
 
-module.exports = () => ({
+const utils = require('@strapi/utils');
+const { ValidationError } = utils.errors;
 
+const { yup, validateYupSchema } = require('@strapi/utils');
+const validateSchema = yup.object().shape({
+  skus: yup.array().of(
+    yup.object().shape({
+      id: yup.number().required(),
+      length: yup.number().required(),
+    })
+  ),
+  note: yup.string(),
+  isDebt: yup.boolean(),
+  deliveryInfo: yup.object().required().shape({
+    email: yup.string().email().required(),
+    firstname: yup.string().required(),
+    lastname: yup.string().required(),
+    address: yup.string().required(),
+    wardId: yup.number().required(),
+  }),
+  deliveryMethod: yup.number().required(),
+});
+
+module.exports = () => ({
+  async process(user, data) {
+    await validateYupSchema(validateSchema)(data);
+    
+    let { note, isDebt, deliveryInfo, deliveryMethod } = data;
+    const cartData = await strapi.service('api::cart.cart-step-cart').process(user, data);
+
+    const deliveryMethods = await strapi.service('api::cart.cart-step-delivery').getDeliveryMethods(); 
+    const realDeliveryMethod = deliveryMethods.filter((item) => item.id == deliveryMethod);
+    if (realDeliveryMethod.length == 0) {
+      throw new ValidationError('Invalid delivery method');
+    }
+
+    let returnData = {
+      ...cartData,
+      note: note ? note : '',
+      isDebt: isDebt ? isDebt : false,
+      deliveryInfo: deliveryInfo,
+      deliveryMethod: realDeliveryMethod[0],
+    }
+
+    return returnData;
+  }
 });
--- src/api/cart/services/cart-step-delivery.js
+++ src/api/cart/services/cart-step-delivery.js
@@ -38,6 +38,10 @@ const deliveryMethods = [
 ];
 
 module.exports = () => ({
+  async getDeliveryMethods() {
+    return deliveryMethods;
+  },
+
   async process(user, data) {
     await validateYupSchema(validateSchema)(data);
     
