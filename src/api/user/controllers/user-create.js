048911754d3170e2f3766da716ae2dcdae00bcde
Luu <ngthluu@gmail.com>
2022-04-29 16:39:57 +0700

    User branch

--- src/api/user/controllers/user-create.js
+++ src/api/user/controllers/user-create.js
@@ -8,6 +8,7 @@ module.exports = {
   create: async (ctx, next) => {
     try {
       const { data } = ctx.request.body;
+      strapi.log.info(JSON.stringify(data));
       await strapi.service('api::user.user-create').create(data); 
       ctx.body = 'ok';
     } catch (err) {
