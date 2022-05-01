7452fba1f878304c99d443983dcefe302dbc99ed
Luu <ngthluu@gmail.com>
2022-05-01 17:20:07 +0700

    Init user leave

---
+++ src/api/user-leave/routes/user-leave.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-leave router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::user-leave.user-leave');
