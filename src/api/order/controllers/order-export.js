0abc21c1cb731971e12f23bfadcc369ab78605bd
Luu <ngthluu@gmail.com>
2022-05-01 13:41:40 +0700

    Finish order export excel

--- src/api/order/controllers/order-export.js
+++ src/api/order/controllers/order-export.js
@@ -24,7 +24,29 @@ module.exports = {
       debt_amount: 'Nợ (đ)',
     }
 
-    const dataset = [];
+    const dataset = data.map((item) => {
+      return {
+        id: `${item.type.toUpperCase()}#${item.id}`,
+        invoice_id: item.order_invoice ? `S-INVOICE#${item.order_invoice.id}` : '',
+        branch_name: item.branch ? item.branch.name : '',
+        member_email: item.customer ? item.customer.email : '',
+        create_date: item.createdAt,
+        status: item.order_statuses.sort((a, b) => Date.parse(a.createdAt) < Date.parse(b.createdAt) ? 1 : -1)[0].status,
+        total_amount: item.order_invoice
+          ? item.order_invoice.price
+          : 0,
+        paid_amount: item.order_invoice
+          ? item.order_invoice.order_payment_invoices.reduce(
+            (prev, cur) => prev + parseFloat(cur.amount), 0
+          ) 
+          : 0,
+        debt_amount: item.order_invoice
+          ? item.order_invoice.price - item.order_invoice.order_payment_invoices.reduce(
+            (prev, cur) => prev + parseFloat(cur.amount), 0
+          )
+          : 0,
+      }
+    });
       
     await strapi.service('api::export-excel.export-excel').exportExcel(ctx, headers, dataset);
   }
