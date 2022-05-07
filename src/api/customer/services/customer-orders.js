ae8b4a01e77643635417dde0d7195ae56088af37
Luu <ngthluu@gmail.com>
2022-05-08 01:22:23 +0700

    Fix a bug

--- src/api/customer/services/customer-orders.js
+++ src/api/customer/services/customer-orders.js
@@ -6,7 +6,9 @@ const processOrdersData = (orders) => {
   }).map((item) => {
     return {
       code: `ORDER#${item.id}`,
-      orderAmount: item.products.reduce((sum, _) => sum + parseInt(_.inventory_item.sku_quantity.sku.price) * parseInt(_.length) * 0.01, 0),
+      orderAmount: item.products.reduce(
+        (sum, _) => sum + parseInt(_.inventory_item.sku_quantity.sku.price) * parseInt(_.length) * 0.01, 
+      item.delivery_method ? item.delivery_method.amount : 0),
       invoiceAmount: item.order_invoice ? item.order_invoice.price : 0,
       ...item,
     }
