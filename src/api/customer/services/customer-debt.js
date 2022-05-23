aa454d3f4443181b8c6c434e9d8f0d0ed9e2bc8b
Luu <ngthluu@gmail.com>
2022-05-23 14:50:14 +0700

    Fix debt

--- src/api/customer/services/customer-debt.js
+++ src/api/customer/services/customer-debt.js
@@ -2,32 +2,32 @@
 
 module.exports = () => ({
     async updateDebt(id, data) {
-        let amount = parseInt(data.amount);
-        const customer = await strapi.entityService.findOne('plugin::users-permissions.user', id, {
-            populate: ['orders', 'orders.order_invoice', 'orders.order_invoice.order_payment_invoices']
+        let customerOrders = await strapi.entityService.findMany('api::order.order', {
+            filters: { customer: { id: { $eq: id } } },
+            populate: ['order_invoice', 'order_invoice.order_payment_invoices']
         });
-        const orders = customer.orders.map(order => {
-            const amount = order.order_invoice.price
-            const paid = order.order_invoice.order_payment_invoices.reduce((prev, cur) => prev + parseInt(cur.amount), 0)
-            const debt = amount - paid
+        customerOrders = customerOrders.map((order) => {
+            const orderInvoice = order.order_invoice;
+            const total = orderInvoice.price;
+            const paid = orderInvoice.order_payment_invoices.reduce((_, invoice) => _ + parseInt(invoice.amount), 0);
+            const debt = total - paid;
             return {
-                invoice_id: order.order_invoice.id,
-                amount: amount,
-                paid: paid,
+                invoiceId: orderInvoice.id,
                 debt: debt,
             }
-        }).sort((a, b) => b.debt - a.debt);
-        for (let order of orders) {
-            order.new_paid = (order.debt - amount) >= 0 ? amount : order.debt;
-            amount -= order.debt;
-            if (amount <= 0) break;
+        });
+        let debtPayAmount = parseInt(data.amount);
+        for (let order of customerOrders) {
+            order.paid = (order.debt - debtPayAmount) >= 0 ? debtPayAmount : order.debt;
+            debtPayAmount -= order.debt;
+            if (debtPayAmount <= 0) break;
         }
-        for (let order of orders) {
-            if (!order.hasOwnProperty('new_paid')) continue;
+        for (let order of customerOrders) {
+            if (!order.hasOwnProperty('paid')) continue;
             await strapi.entityService.create('api::order-payment-invoice.order-payment-invoice', {
                 data: {
-                    order_invoice: order.invoice_id,
-                    amount: order.new_paid,
+                    order_invoice: order.invoiceId,
+                    amount: order.paid,
                 },
             });
         }
