6d12b393fb4b291a9dbed7313b9f620dea3230a7
Luu <ngthluu@gmail.com>
2022-05-26 01:46:54 +0700

    Fix a bug

--- src/api/export-pdf/services/export-pdf.js
+++ src/api/export-pdf/services/export-pdf.js
@@ -11,32 +11,23 @@ module.exports = () => ({
     const html = fs.readFileSync(templateFileName, "utf8");
 
     const options = {
-      phantomPath: "./node_modules/phantomjs-prebuilt/bin/phantomjs",
       format: "A4",
       orientation: "portrait",
       border: "10mm",
-      localUrlAccess: true,
     };
 
     const document = {
       html: html,
       data: data,
       type: "buffer",
-      path: "./output.pdf",
     };
-    await (new Promise((resolve, reject) => {
-      pdf
-        .create(document, options)
-        .then((pdfBuffer) => {
-          ctx.attachment('output.pdf');
-          ctx.send(pdfBuffer);
-          resolve(true);
-        })
-        .catch((error) => {
-          strapi.log.info(JSON.stringify(error));
-          reject(true);
-        })
-        ;
-    }));
+    
+    try {
+      const pdfBuffer = await pdf.create(document, options);
+      ctx.attachment('output.pdf');
+      ctx.send(pdfBuffer);
+    } catch (err) {
+      strapi.log.info(JSON.stringify(err));
+    }
   }
 });
