e73c2c33282734d72b41d7185d931fc576417067
Luu <ngthluu@gmail.com>
2022-05-29 03:31:42 +0700

    Add discount amount to mail template

--- src/api/cart/services/cart-save.js
+++ src/api/cart/services/cart-save.js
@@ -72,6 +72,7 @@ const sendOrderConfirmationMail = async (order) => {
       return sum + 0.01 * _.length * _.unit_price
     }, 0),
     DELIVERY_COST: order.delivery_method.amount,
+    DISCOUNT_AMOUNT: order.discount_value ? order.discount_value : 0,
   });
 
   try {
