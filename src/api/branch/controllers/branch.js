5962fb9b9d8a9ff52251a5385c436a9a38039256
Luu <ngthluu@gmail.com>
2022-05-28 23:10:09 +0700

    Fix branch

--- src/api/branch/controllers/branch.js
+++ src/api/branch/controllers/branch.js
@@ -6,13 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::branch.branch', ({strap}) => ({
-    async find(ctx) {
-        ctx.query = {
-            ...ctx.query,
-            user: ctx.state.user
-        }
-        const { data, meta } = await super.find(ctx)
-        return { data, meta };
-    }
-}));
+module.exports = createCoreController('api::branch.branch');
--- src/api/branch/services/branch.js
+++ src/api/branch/services/branch.js
@@ -6,22 +6,4 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::branch.branch', ({strapi}) => ({
-    async find(args) {
-        const { user, ...otherArgs } = args
-        const fetchUser = await strapi.service('api::user.user').fetchOne(user.id, [], ['branches'])
-        let fetchArgs = {...otherArgs}
-        if (fetchUser) {
-            fetchArgs = {
-                ...fetchArgs,
-                filters: { 
-                    id: { 
-                        $in: fetchUser.branches.map((item) => item.id) 
-                    } 
-                }
-            }
-        }
-        const { results, pagination } = await super.find(fetchArgs);
-        return { results, pagination }
-    }
-}));
+module.exports = createCoreService('api::branch.branch');
